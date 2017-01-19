a613377963d1ad5ba96775f547dc31a8e44743bc
vaibhav3301 <vaibhavshelke017@gmail.com>
2017-01-20 04:46:18 +0530

    scraper improved

--- scrap/get_quotes.py
+++ scrap/get_quotes.py
@@ -2,9 +2,13 @@ import requests as rq
 from bs4 import BeautifulSoup
 #with open('data.txt') as f:
 
-page = rq.get('https://www.goodreads.com/work/quotes/1492580-bhagavadg-t')
-
+url = 'https://www.goodreads.com/work/quotes/1492580-bhagavadg-t'
+page = rq.get(url)
+data = { "quotes" : [] }
 soup = BeautifulSoup(page.text, 'html.parser')
 
-for i in soup.div['class']:
-	print i
+for i in soup.find_all("div", class_="quoteText"):
+	print i.contents[0]
+	data.update("quote" : "")
+
+print data
