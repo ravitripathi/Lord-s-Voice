859093d94de1bf8c756ff5c1624ad4be964e9e1e
vaibhav3301 <vaibhavshelke017@gmail.com>
2017-02-09 12:49:55 +0530

    data.txt added

--- scrap/get_quotes.py
+++ scrap/get_quotes.py
@@ -1,14 +1,23 @@
 import requests as rq
 from bs4 import BeautifulSoup
+import codecs
 #with open('data.txt') as f:
 
 url = 'https://www.goodreads.com/work/quotes/1492580-bhagavadg-t'
+n = "?page=2"
 page = rq.get(url)
-data = { "quotes" : [] }
+data = []
 soup = BeautifulSoup(page.text, 'html.parser')
 
-for i in soup.find_all("div", class_="quoteText"):
-	print i.contents[0]
-	data.update("quote" : "")
+for i in range(2, 7):
+	for j in soup.find_all("div", class_="quoteText"):
+		data.append(j.contents[0])
+	page = rq.get(url + "?page=" + str(i))
+	soup = BeautifulSoup(page.text, 'html.parser')
+	print "Completed page" + str(i-1)
 
-print data
+with codecs.open('data.txt', 'w', 'utf-8') as f:
+	for k in data:
+		f.write(k)
+
+print "Completed!"
